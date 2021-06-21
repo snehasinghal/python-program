a6b87bc29a1eb76a1e9c74733d02b01d8b7d8fea
Sneha Singhal <76214687+snehasinghal@users.noreply.github.com>
2021-06-21 17:05:37 +0530

    Create bool.py

---
+++ bool.py
@@ -0,0 +1,9 @@
+x='Python'
+y='1'
+z='False'
+
+print(bool(x))         #true
+print(bool(y))         #true
+print(bool(z))         #true
+print(bool(()))        #false
+print(bool([]))        #false
