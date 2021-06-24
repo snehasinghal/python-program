5df50703c2b06181e173caa8ba5d0aad35c82d10
Sneha Singhal <76214687+snehasinghal@users.noreply.github.com>
2021-06-24 21:50:07 +0530

    Create RemoveElement.py

---
+++ Array/RemoveElement.py
@@ -0,0 +1,6 @@
+fruits = ["Apple","Banana","Mango","Papaya","Grapes"]
+fruits2=["Guava","Melon"]
+fruits.extend(fruits2)
+fruits.remove("Melon") #removes the Melon from the array fruits
+fruits.pop(2)           #removes the value at position 2
+print(fruits)
